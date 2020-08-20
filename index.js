f58754df48a3379c9b18c28f251064818d113dae
RoxanaJudith <roxanajpr.4@gmail.com>
2020-08-20 18:56:09 -0400

    Modulos - exports

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-function add(x1, x2){
-    return x1 + x2;
-}
-function substract(x1, x2){
-    return x1 - x2;
-}
-function multiply(x1, x2){
-    return x1 * x2;
-}
-function divide(x1, x2){
-    if(x2 == 0){
-        console.log('No se puede dividir entre 0');
-    }else
-        return x1 / x2;
-    
-}
-console.log(divide(1,0));
+const math = require('./math.js')
+//console.log(math);  math es un objeto con multiples propiedades
+
+console.log(math.suma(1,0));
+console.log(math.resta(1,0));
+console.log(math.divide(1,0));
